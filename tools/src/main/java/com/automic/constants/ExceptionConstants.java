34cf63cbc93f1a18d3c5154268ad716e623ebf2f
NG\anuragupadhyay <rag.anu01@gmail.com>
2021-03-19 14:44:31 +0530

    PMARA-17875: Review Comment java code change commit

--- tools/src/main/java/com/automic/constants/ExceptionConstants.java
+++ tools/src/main/java/com/automic/constants/ExceptionConstants.java
@@ -7,7 +7,19 @@ package com.automic.constants;
 public class ExceptionConstants {
 
 	
-	public static final String INVALID_FILE_TYPE = "Invalid file extension provided [%s]. Rquired PNG file type";    
+    public static final String INVALID_FILE_TYPE = "Invalid file type provided [%s], required PNG file type";
+    public static final String INVALID_JSON = "File doesn't have correct Json";
+		
+    public static final String UNABLE_TO_FLUSH_STREAM = "Error while flushing stream";
+    public static final String INVALID_ARGS = "Improper Args. Possible cause : %s";
+
+    public static final String INVALID_INPUT_PARAMETER = "Invalid value for parameter [%s] : [%s]";
+
+    public static final String GENERIC_ERROR_MSG = "System Error occured.";
+
+ // Certificate errors
+    public static final String ERROR_SKIPPING_CERT = "Error skipping the certificate validation";
+    
     
     private ExceptionConstants() {
     }
